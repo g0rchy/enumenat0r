a49ae2bea378f3633af3512ef3a4b3c0ebe5994a
g0rchy <g0rchy@10.13.15.223>
2021-12-14 12:50:15 +0100

    added autoupdating DNS resolvers & fixed some typos

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Enumenat0r
+# enumenat0r
 Just another subdomain enumeration script :3
 
 It's just a wrapper around some subdomain enumeration and fingerprinting tools, with some concurrency to save some time, not the best but it gets the job done.
@@ -7,7 +7,7 @@ All the tools' output are stored in these files:
   - `domains.txt` contains all enumerated subdomains.
   - `live.txt` contains all live subdomains.
   - `gau.txt` contains the output of gau.
-# Used Tools:
+# Used Tools/Repos:
   - [Assetfinder](https://github.com/tomnomnom/assetfinder)
   - [Httprobe](https://github.com/tomnomnom/httprobe)
   - [Amass](https://github.com/OWASP/Amass)
@@ -15,7 +15,8 @@ All the tools' output are stored in these files:
   - [Aquatone](https://github.com/michenriksen/aquatone)
   - [httpx](https://github.com/projectdiscovery/httpx)
   - [gau](https://github.com/lc/gau)
+  - [resolvers](https://github.com/janmasarik/resolvers)
 # Notes:
   You have to change the path variable with the path of your own DNS resolvers list to get amass working.
-  
-  Also make sure to tweak the number of threads of the tools based on your machine/need.
+ 
+  Also make sure to tweak the number of threads of the tools based on your machine/needs
--- enumenat0r.sh
+++ enumenat0r.sh
@@ -10,6 +10,9 @@ fi
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+echo -e "\n\e[0;32m[+]\e[0m Updating your resolvers list..."
+wget https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt -o $path
+
 for domain in "$@"
 do
 	subfinder -d $domain -silent -recursive -all -t 200 >>  domains.txt &
@@ -30,16 +33,18 @@ sort -u live.txt -o live.txt
 live=$(wc -l live.txt| cut -d ' ' -f 1)
 echo -e "\n\e[0;32m[!]\e[0m Done:\n\t\e[1;33m[*]\e[0m Total discovered (sub)domains: $(wc -l domains.txt|cut -d ' ' -f 1)\n\t\e[1;33m[*]\e[0m Total live (sub)domains: $live\n"
 
-prompt=$(echo -e "\e[0;31m[!]\e[0m The scope is large and it might take sometime to do domain flyover, want to do light fingerprinting instead? (y/n/q): ")
+prompt=$(echo -e "\e[0;31m[!]\e[0m The scope is large and it might take some time to do domain flyover, want to do light fingerprinting instead? (y/n/q): ")
 if [ $live -gt 1 ]
 	then
-		while true; do
-			read -p "$prompt" yn
-	    	case $yn in
-	        	[Yy]* ) echo -e "\n\e[0;32m[+]\e[0m Starting Fingerprinting... (results are stored under fingerprint.out)"; cat live.txt | httpx -status-code -title -content-length -follow-redirects -tech-detect | tee -a fingerprint.out; break;;
-        		[Nn]* ) echo -e "\n\e[0;32m[+]\e[0m Running aquatone..."; cat live.txt | aquatone -silent; break;;
+		while true
+			do
+				read -p "$prompt" yn
+			    	case $yn in
+		        	[Yy]* ) echo -e "\n\e[0;32m[+]\e[0m Starting Fingerprinting... (results are stored under fingerprint.out)"; cat live.txt | httpx -status-code -title -content-length -follow-redirects -tech-detect | tee -a fingerprint.out; break;;
+	        		[Nn]* ) echo -e "\n\e[0;32m[+]\e[0m Running aquatone..."; cat live.txt | aquatone -silent; break;;
 				[Qq]* ) exit;;
-        		* ) echo "Please answer y or n";;
-    		esac
-		done
+	        		* ) echo "Please answer  with y or n";;
+    				esac
+			done
 fi
+
