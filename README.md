66992f886f19df24152a4640049a08e53d4960c6
g0rchy <g0rchy@10.13.15.223>
2021-12-14 14:21:12 +0100

    tweaked wget a little bit & fixed the comparison

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # enumenat0r
 Just another subdomain enumeration script :3
 
-It's just a wrapper around some subdomain enumeration and fingerprinting tools, with some concurrency to save some time, not the best but it gets the job done.
+It's just a wrapper around some subdomain enumeration and fingerprinting tools, with some parallelization to save some time, not the best but it gets the job done.
 
 All the tools' output are stored in these files:
   - `domains.txt` contains all enumerated subdomains.
   - `live.txt` contains all live subdomains.
   - `gau.txt` contains the output of gau.
+  - `fingerprint.out` contains httpx fingerprint results.
 # Used Tools/Repos:
   - [Assetfinder](https://github.com/tomnomnom/assetfinder)
   - [Httprobe](https://github.com/tomnomnom/httprobe)
@@ -17,6 +18,11 @@ All the tools' output are stored in these files:
   - [gau](https://github.com/lc/gau)
   - [resolvers](https://github.com/janmasarik/resolvers)
 # Notes:
-  You have to change the path variable with the path of your own DNS resolvers list to get amass working.
+  You have to change the `path` variable with the path of in which you'd like to store the DNS resolvers list to get amass working.
  
-  Also make sure to tweak the number of threads of the tools based on your machine/needs
+  Also make sure to tweak the number of threads of the tools based on your machine/needs.
+
+# TODO:
+  - More efficent parallelization (e.g gnu parallel)
+  - More tools & sources
+  - Subdomain bruteforcing 
--- enumenat0r.sh
+++ enumenat0r.sh
@@ -11,36 +11,35 @@ fi
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 echo -e "\n\e[0;32m[+]\e[0m Updating your resolvers list..."
-wget https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt -o $path
+wget -q https://raw.githubusercontent.com/janmasarik/resolvers/master/resolvers.txt -O $path
 
 for domain in "$@"
 do
 	subfinder -d $domain -silent -recursive -all -t 200 >>  domains.txt &
-	assetfinder --subs-only $domain >> domains.txt &
+	assetfinder --subs-only $domain  >> domains.txt &
 	amass enum --passive -rf $path -d $domain >> domains.txt &
 	gau -subs $domain >> gau.txt &
 done
-wait
 
 echo -e "\n\e[0;32m[+]\e[0m Passive enumeration done"
-cat gau.txt | cut -d / -f 3 >> domains.txt
+cat gau.txt | cut -d '/' -f 3 >> domains.txt
 sort -u domains.txt -o domains.txt
 
 echo -e "\n\e[0;32m[+]\e[0m Probing for live domains..."
-cat domains.txt | httprobe >> live.txt 
+cat domains.txt | httprobe -c 100 >> live.txt 
 sort -u live.txt -o live.txt
 
 live=$(wc -l live.txt| cut -d ' ' -f 1)
 echo -e "\n\e[0;32m[!]\e[0m Done:\n\t\e[1;33m[*]\e[0m Total discovered (sub)domains: $(wc -l domains.txt|cut -d ' ' -f 1)\n\t\e[1;33m[*]\e[0m Total live (sub)domains: $live\n"
 
 prompt=$(echo -e "\e[0;31m[!]\e[0m The scope is large and it might take some time to do domain flyover, want to do light fingerprinting instead? (y/n/q): ")
-if [ $live -gt 1 ]
+if [ $live -gt 100 ]
 	then
 		while true
 			do
 				read -p "$prompt" yn
 			    	case $yn in
-		        	[Yy]* ) echo -e "\n\e[0;32m[+]\e[0m Starting Fingerprinting... (results are stored under fingerprint.out)"; cat live.txt | httpx -status-code -title -content-length -follow-redirects -tech-detect | tee -a fingerprint.out; break;;
+		        	[Yy]* ) echo -e "\n\e[0;32m[+]\e[0m Starting Fingerprinting... (results are stored under fingerprint.out)"; cat live.txt | httpx -status-code -title -content-length -follow-redirects -tech-detect -threads 100 | tee -a fingerprint.out; break;;
 	        		[Nn]* ) echo -e "\n\e[0;32m[+]\e[0m Running aquatone..."; cat live.txt | aquatone -silent; break;;
 				[Qq]* ) exit;;
 	        		* ) echo "Please answer  with y or n";;
